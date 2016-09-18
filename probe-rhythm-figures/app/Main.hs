a1093c91e72e0885f95f30445291f242fc94f627
Michal Hadrava <mihadra@gmail.com>
2016-09-19 00:56:09 +0200

    Final touches on the figures based on Bahňa's suggestions.

--- probe-rhythm-figures/app/Main.hs
+++ probe-rhythm-figures/app/Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
+import Data.List as L
 
 import Lib
 
@@ -12,26 +14,39 @@ main = multiMain
   ]
 
 polyRhythms :: Diagram B
-polyRhythms = pad 1.1 . centerXY $ hsep 4
-  [vsep 4 $ text lbl : [centerXY $ rhythmsWithSync w0 pc tn f0 | pc <- pcs] | (lbl, tn) <- tns]
+polyRhythms = frame 1 . centerXY $ vsep 4 allOscs
   where pbs = [[], [(red, 2)], [(red, 1)]]
         cxt = [(black, 0), (black, 4), (black, 7)]
         pcs = map (cxt ++) pbs
         w0  = 15
         f0  = 1
         tns = [("12TET", eqt), ("Farey", farey)]
+        sp  = 2
+        eqtOscs  = [ beside' unitX (centerY (labelOscs sp pc) ||| strutX 2)
+                                   (centerXY $ rhythmsWithSync sp w0 pc eqt f0)
+                   | pc <- pcs]
+        farOscs  = [centerXY $ rhythmsWithSync sp w0 pc farey f0 | pc <- pcs]
+        eqtOscs' = beside' (-unitY) (centerX (text "12TET") ||| strutY 2) (head eqtOscs) : tail eqtOscs
+        farOscs' = beside' (-unitY) (centerX (text "Farey") ||| strutY 2) (head farOscs) : tail farOscs
+        allOscs  = zipWith (\e f -> hsep 4 [e, f]) eqtOscs' farOscs'
 
 tonesToRhythms :: Diagram B
-tonesToRhythms = pad 1.1 . centerXY $ hsep 4
-  [ vsp [text "original chord", tones w1 pcs tn f1, chord w1 pcs tn f1, text "~0.1 s"]
-  , vsp [text "frequencies scaled down", tones w2 pcs tn f2, chord w2 pcs tn f2, text "~10 s"]
-  , vsp [text "waves transformed to ticks", rhythms w2 pcs tn f2, polyRhythm w2 pcs tn f2, text "~10 s"]
-  ]
-  where w0  = 7
-        pcs = [(black, 0), (black, 4), (black, 7), (red, 11)]
-        tn  = eqt
-        f1  = 5
-        f2  = 1
-        w1  = w0 * f1
-        w2  = w0 * f2
-        vsp = vsep 2 . map centerXY
+tonesToRhythms = frame 1 . centerXY $ hsep 2 oscs'
+  where w0   = 7
+        pcs  = [(black, 0), (black, 4), (black, 7), (red, 11)]
+        tn   = eqt
+        f1   = 4
+        f2   = 1
+        w1   = w0 * f1
+        w2   = w0 * f2
+        sp   = 2
+        titles    = map (centerX . text)
+                        ["original chord", "frequencies scaled down", "waves transformed to ticks"]
+        oscs      = [ beside' unitX (centerY (labelOscs sp pcs) ||| strutX 1) (centerXY $ tones sp w1 pcs tn f1)
+                    , centerXY $ tones sp w2 pcs tn f2, centerXY $ rhythms sp w2 pcs tn f2
+                    ]
+        polyOscs  = [ beside' unitX (centerY (labelPolyOsc sp pcs) ||| strutX 1) (centerXY $ chord sp w1 pcs tn f1)
+                    , centerXY $ chord sp w2 pcs tn f2, centerXY $ polyRhythm sp w2 pcs tn f2
+                    ]
+        oscScales = map centerX [oscScale f1 "0.01 s", oscScale f2 "1 s", oscScale f2 "1 s"]
+        oscs'     = zipWith4 (\t o p s -> vsep 1 [t, o, p, s]) titles oscs polyOscs oscScales
--- probe-rhythm-figures/src/Lib.hs
+++ probe-rhythm-figures/src/Lib.hs
@@ -4,11 +4,15 @@
 module Lib
     ( vdist
     , hdist
+    , beside'
     , tones
     , rhythms
     , chord
     , polyRhythm
     , rhythmsWithSync
+    , labelOscs
+    , labelPolyOsc
+    , oscScale
     , farey
     , eqt
     ) where
@@ -31,6 +35,11 @@ vdist = dist vcat'
 
 hdist :: Double -> [Diagram B] -> Diagram B
 hdist = dist hcat'
+
+beside' = flip . beside . negated
+
+frameH :: Double -> Diagram B -> Diagram B
+frameH h d = d # centerXY `atop` rect (width d) h # lw none
 
 toneHalfCycle :: Trail V2 Double
 toneHalfCycle = trailFromSegments [bezier3 (0.5 D.*^ unitX) (r2 (0.5, -1)) (r2 (1, -1))]
@@ -59,31 +68,27 @@ tone = osc toneCycle (0, 1)
 rhythm :: Double -> Trail V2 Double
 rhythm = osc rhythmCycle (0.25, 0.75)
 
-polyOsc :: (Double -> Trail V2 Double) -> (Double -> [(Colour Double, Trail V2 Double)] -> Diagram B) -> (Double -> [Diagram B] -> Diagram B) -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-polyOsc oscFn combFn lblFn w0 pcs tn f0 = hdist sepa
-  [lblFn sepa lbl # translateY (-0.5), combFn sepa oscs # scaleX (1 / f0')]
-  where pcs' = sortOn (negate . snd) pcs
-        fs   = P.map (second (\pc -> tn ! pc)) pcs'
+polyOsc :: (Double -> Trail V2 Double) -> (Double -> [(Colour Double, Trail V2 Double)] -> Diagram B) -> Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+polyOsc oscFn combFn sepa w0 pcs tn f0 = combFn sepa oscs # scaleX (1 / f0')
+  where fs   = P.map (second (\pc -> tn ! pc)) $ sortOn (negate . snd) pcs
         oscs = P.map (second (\f -> oscFn (w0' * f) # scaleX (1 / f))) fs
         w0'  = fromIntegral w0
         f0'  = fromIntegral f0
-        nt   = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-        lbl  = P.map (\(col, pc) -> alignedText 0 0.5 (nt !! pc) # lw none # fc col) pcs'
-        sepa = 2
 
 oscs :: (Trail V2 Double -> Diagram B) -> Double -> [(Colour Double, Trail V2 Double)] -> Diagram B
 oscs strokeFn sepa = vdist sepa . P.map (\(col, osc) -> strokeFn osc # lc col)
 
 chord' :: (Trail V2 Double -> Diagram B) -> Double -> [(Colour Double, Trail V2 Double)] -> Diagram B
-chord' _ _ oscs = (cubicSpline False chdSmps :: Trail V2 Double) # stroke
+chord' _ _ oscs = (cubicSpline False chdSmps :: Trail V2 Double) # stroke # frameH nOscs
   where oscs'   = snd $ unzip oscs
+        nOscs   = fromIntegral $ length oscs
         maxLen  = length . trailSegments $ head oscs'
         par     = toList $ linspace (4 * maxLen) (0, 1)
         oscSmps = L.transpose [[osc `atParam` p | p <- par] | osc <- P.map (`at` origin) oscs']
         chdSmps = P.map (foldl (\pt pt' -> (pt' ^. _x) ^& (pt ^. _y + pt' ^. _y)) origin) oscSmps
 
 polyRhythm' :: (Trail V2 Double -> Diagram B) -> Double -> [(Colour Double, Trail V2 Double)] -> Diagram B
-polyRhythm' strokeFn _ oscs = scaleY nOscs . mconcat . P.map strokeFn . snd $ unzip oscs
+polyRhythm' strokeFn _ oscs = frameH nOscs . mconcat . P.map strokeFn . snd $ unzip oscs
   where nOscs = fromIntegral $ length oscs
 
 syncCurve :: V2 Double -> Trail V2 Double
@@ -108,29 +113,41 @@ strokeRhythm cyc = hcat $ P.zipWith ($) strokeFns segments
   where segments  = explodeTrail $ cyc `at` origin
         strokeFns = cycle (strokeLocTrail : replicate 3 (strokeLocTrail # lw none))
 
-lblOscs :: Double -> [Diagram B] -> Diagram B
-lblOscs = vdist
+label :: (Double -> [Diagram B] -> Diagram B) -> Double -> [(Colour Double, Int)] -> Diagram B
+label lblFn sepa pcs = lblFn sepa lbl
+  where pcs' = sortOn (negate . snd) pcs
+        nt   = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+        lbl  = P.map (\(col, pc) -> alignedText 0 0.5 (nt !! pc) # lw none # fc col) pcs'
 
-tones :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-tones = polyOsc tone (oscs strokeTone) lblOscs
+labelOscs :: Double -> [(Colour Double, Int)] -> Diagram B
+labelOscs = label vdist
 
-rhythms :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-rhythms = polyOsc rhythm (oscs strokeRhythm) lblOscs
+tones :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+tones = polyOsc tone (oscs strokeTone)
 
-lblPolyOsc :: Double -> [Diagram B] -> Diagram B
-lblPolyOsc _ = vdist 1
+rhythms :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+rhythms = polyOsc rhythm (oscs strokeRhythm)
 
-chord :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-chord = polyOsc tone (chord' strokeTone) lblPolyOsc
+labelPolyOsc :: Double -> [(Colour Double, Int)] -> Diagram B
+labelPolyOsc _ = label vdist 1
 
-polyRhythm :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-polyRhythm = polyOsc rhythm (polyRhythm' strokeRhythm) lblPolyOsc
+chord :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+chord = polyOsc tone (chord' strokeTone)
 
-rhythmSync :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-rhythmSync = polyOsc rhythm (rhythmSync' strokeRhythm) (\_ _ -> mempty)
+polyRhythm :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+polyRhythm = polyOsc rhythm (polyRhythm' strokeRhythm)
 
-rhythmsWithSync :: Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
-rhythmsWithSync w0 pcs tn f0 = rhythms w0 pcs tn f0 `atop` rhythmSync w0 pcs tn f0
+rhythmSync :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+rhythmSync = polyOsc rhythm (rhythmSync' strokeRhythm)
+
+rhythmsWithSync :: Double -> Int -> [(Colour Double, Int)] -> Vector Double -> Int -> Diagram B
+rhythmsWithSync sepa w0 pcs tn f0 = rhythms sepa w0 pcs tn f0 `atop` rhythmSync sepa w0 pcs tn f0
+
+oscScale :: Int -> String -> Diagram B
+oscScale f0 txt = vsep 1 [glyph, txt']
+  where glyph =   map straight [-0.5 *^ unitY, unitX, 0.5 *^ unitY] # fromSegments
+                # strokeTrail # scaleX (2 / fromIntegral f0) # centerXY
+        txt'  = text txt # centerXY
 
 farey :: Vector Double
 farey = fromList [1, 16/15, 9/8, 6/5, 5/4, 4/3, 17/12, 3/2, 8/5, 5/3, 16/9, 15/8]
