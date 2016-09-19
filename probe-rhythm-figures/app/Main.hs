64b0cdd53ab30bb7d14091d54e519201034c8043
Michal Hadrava <mihadra@gmail.com>
2016-09-19 02:16:34 +0200

    Reduced spacing a bit.

--- probe-rhythm-figures/app/Main.hs
+++ probe-rhythm-figures/app/Main.hs
@@ -14,7 +14,7 @@ main = multiMain
   ]
 
 polyRhythms :: Diagram B
-polyRhythms = frame 1 . centerXY $ vsep 4 allOscs
+polyRhythms = frame 1 . centerXY $ vsep 3 allOscs
   where pbs = [[], [(red, 2)], [(red, 1)]]
         cxt = [(black, 0), (black, 4), (black, 7)]
         pcs = map (cxt ++) pbs
@@ -22,16 +22,16 @@ polyRhythms = frame 1 . centerXY $ vsep 4 allOscs
         f0  = 1
         tns = [("12TET", eqt), ("Farey", farey)]
         sp  = 2
-        eqtOscs  = [ beside' unitX (centerY (labelOscs sp pc) ||| strutX 2)
+        eqtOscs  = [ beside' unitX (centerY (labelOscs sp pc) ||| strutX 1.5)
                                    (centerXY $ rhythmsWithSync sp w0 pc eqt f0)
                    | pc <- pcs]
         farOscs  = [centerXY $ rhythmsWithSync sp w0 pc farey f0 | pc <- pcs]
-        eqtOscs' = beside' (-unitY) (centerX (text "12TET") ||| strutY 2) (head eqtOscs) : tail eqtOscs
-        farOscs' = beside' (-unitY) (centerX (text "Farey") ||| strutY 2) (head farOscs) : tail farOscs
-        allOscs  = zipWith (\e f -> hsep 4 [e, f]) eqtOscs' farOscs'
+        eqtOscs' = beside' (-unitY) (centerX (text "12TET") ||| strutY 3) (head eqtOscs) : tail eqtOscs
+        farOscs' = beside' (-unitY) (centerX (text "Farey") ||| strutY 3) (head farOscs) : tail farOscs
+        allOscs  = zipWith (\e f -> hsep 3 [e, f]) eqtOscs' farOscs'
 
 tonesToRhythms :: Diagram B
-tonesToRhythms = frame 1 . centerXY $ hsep 4 oscs'
+tonesToRhythms = frame 1 . centerXY $ hsep 3 oscs'
   where w0   = 7
         pcs  = [(black, 0), (black, 4), (black, 7), (red, 11)]
         tn   = eqt
@@ -42,11 +42,11 @@ tonesToRhythms = frame 1 . centerXY $ hsep 4 oscs'
         sp   = 2
         titles    = map (centerX . text)
                         ["original chord", "frequencies scaled down", "waves transformed to ticks"]
-        oscs      = [ beside' unitX (centerY (labelOscs sp pcs) ||| strutX 2) (centerXY $ tones sp w1 pcs tn f1)
+        oscs      = [ beside' unitX (centerY (labelOscs sp pcs) ||| strutX 1.5) (centerXY $ tones sp w1 pcs tn f1)
                     , centerXY $ tones sp w2 pcs tn f2, centerXY $ rhythms sp w2 pcs tn f2
                     ]
-        polyOscs  = [ beside' unitX (centerY (labelPolyOsc sp pcs) ||| strutX 2) (centerXY $ chord sp w1 pcs tn f1)
+        polyOscs  = [ beside' unitX (centerY (labelPolyOsc sp pcs) ||| strutX 1.5) (centerXY $ chord sp w1 pcs tn f1)
                     , centerXY $ chord sp w2 pcs tn f2, centerXY $ polyRhythm sp w2 pcs tn f2
                     ]
         oscScales = map centerX [oscScale f1 "0.01 s", oscScale f2 "1 s", oscScale f2 "1 s"]
-        oscs'     = zipWith4 (\t o p s -> vsep 2 [t, o, p, s]) titles oscs polyOscs oscScales
+        oscs'     = zipWith4 (\t o p s -> vsep 1.5 [t, o, p, s]) titles oscs polyOscs oscScales
