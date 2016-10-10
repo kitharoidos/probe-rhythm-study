5f06f2071331d33a22ba50c8a77171b46bed42ed
Michal Hadrava <mihadra@gmail.com>
2016-10-10 15:59:30 +0200

    Tweaked font size.

--- probe-rhythm-figures/app/Main.hs
+++ probe-rhythm-figures/app/Main.hs
@@ -14,7 +14,7 @@ main = multiMain
   ]
 
 polyRhythms :: Diagram B
-polyRhythms = frame 1 . centerXY $ vsep 3 allOscs
+polyRhythms = frame 1 . fontSizeO 24 . centerXY $ vsep 3 allOscs
   where pbs = [[], [(red, 2)], [(red, 1)]]
         cxt = [(black, 0), (black, 4), (black, 7)]
         pcs = map (cxt ++) pbs
@@ -22,16 +22,16 @@ polyRhythms = frame 1 . centerXY $ vsep 3 allOscs
         f0  = 1
         tns = [("12TET", eqt), ("Farey", farey)]
         sp  = 2
-        eqtOscs  = [ beside' unitX (centerY (labelOscs sp pc) ||| strutX 1.5)
+        eqtOscs  = [ beside' unitX (centerY (labelOscs sp pc) ||| strutX 2)
                                    (centerXY $ rhythmsWithSync sp w0 pc eqt f0)
                    | pc <- pcs]
         farOscs  = [centerXY $ rhythmsWithSync sp w0 pc farey f0 | pc <- pcs]
-        eqtOscs' = beside' (-unitY) (centerX (text "12TET") ||| strutY 3) (head eqtOscs) : tail eqtOscs
-        farOscs' = beside' (-unitY) (centerX (text "Farey") ||| strutY 3) (head farOscs) : tail farOscs
+        eqtOscs' = beside' (-unitY) (fontSizeO 32 $ centerX (text "12TET") ||| strutY 3) (head eqtOscs) : tail eqtOscs
+        farOscs' = beside' (-unitY) (fontSizeO 32 $ centerX (text "Farey") ||| strutY 3) (head farOscs) : tail farOscs
         allOscs  = zipWith (\e f -> hsep 3 [e, f]) eqtOscs' farOscs'
 
 tonesToRhythms :: Diagram B
-tonesToRhythms = frame 1 . centerXY $ hsep 3 oscs'
+tonesToRhythms = frame 1 . fontSizeO 24 . centerXY $ hsep 3 oscs'
   where w0   = 7
         pcs  = [(black, 0), (black, 4), (black, 7), (red, 11)]
         tn   = eqt
@@ -40,8 +40,8 @@ tonesToRhythms = frame 1 . centerXY $ hsep 3 oscs'
         w1   = w0 * f1
         w2   = w0 * f2
         sp   = 2
-        titles    = map (centerX . text)
-                        ["original chord", "frequencies scaled down", "waves transformed to ticks"]
+        titles    = map (centerX . fontSizeO 32 . text)
+                        ["Original chord", "Frequencies scaled down", "Waves transformed to ticks"]
         oscs      = [ beside' unitX (centerY (labelOscs sp pcs) ||| strutX 1.5) (centerXY $ tones sp w1 pcs tn f1)
                     , centerXY $ tones sp w2 pcs tn f2, centerXY $ rhythms sp w2 pcs tn f2
                     ]
